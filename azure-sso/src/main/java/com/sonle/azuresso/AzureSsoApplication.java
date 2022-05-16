3313f2f3d0da572349027b4f35214e4a1f45c60b
sonle <sonle1997@gmail.com>
2022-05-16 13:39:18 +0700

    init project layout

---
+++ azure-sso/src/main/java/com/sonle/azuresso/AzureSsoApplication.java
@@ -0,0 +1,13 @@
+package com.sonle.azuresso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSsoApplication.class, args);
+	}
+
+}
---
+++ azure-sso/src/test/java/com/sonle/azuresso/AzureSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonle.azuresso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
